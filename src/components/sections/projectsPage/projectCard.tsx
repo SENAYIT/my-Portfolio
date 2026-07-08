704206a0dd3e27bddd65ee10a674c55b8766ff97
senayit <senaitaweke2019@gmail.com>
2026-07-08 04:36:11 +0300

    project sample image added

--- src/components/sections/projectsPage/projectCard.tsx
+++ src/components/sections/projectsPage/projectCard.tsx
@@ -21,7 +21,25 @@ export default function Projectcard({ project }: { project: Project }) {
     <ProjectCardUI>
       <div className="flex flex-col gap-2">
         {/* for image*/}
+        <div>
+          <Image
+            src="/myPortfolioFavicon.png"
+            width={300}
+            height={50}
+            className="w-auto h-auto hidden md:block"
+            alt="projects Image"
+            loading="eager"
+          />
 
+          <Image
+            src="/myPortfolioFavicon.png"
+            width={200}
+            height={50}
+            className="w-auto h-auto block md:hidden"
+            alt="projects Image"
+            loading="eager"
+          />
+        </div>
         {/* project for content */}
 
         <h3 className="text-white">{project.name}</h3>
